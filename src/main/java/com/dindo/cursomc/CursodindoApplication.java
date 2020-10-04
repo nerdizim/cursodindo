5a3119e0d365d9d6a5712a252c4ef32b5129a891
Cassio Mendes <cassio.henrique14@gmail.com>
2020-10-04 16:20:56 -0300

    Projeto criado

---
+++ src/main/java/com/dindo/cursomc/CursodindoApplication.java
@@ -0,0 +1,13 @@
+package com.dindo.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursodindoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursodindoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dindo/cursomc/CursodindoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dindo.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursodindoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
