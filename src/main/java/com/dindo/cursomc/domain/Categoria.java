132946f58f7a1e311d15ce6719733ed00e97054d
Cassio Mendes <cassio.henrique14@gmail.com>
2020-10-06 17:13:18 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/dindo/cursomc/domain/Categoria.java
+++ src/main/java/com/dindo/cursomc/domain/Categoria.java
@@ -2,14 +2,22 @@ package com.dindo.cursomc.domain;
 
 import java.io.Serializable;
 
-public class Categoria implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	
-	public Categoria(){
-		
+
+	public Categoria() {
+
 	}
 
 	public Categoria(Integer id, String nome) {
@@ -58,6 +66,5 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
+
 }
