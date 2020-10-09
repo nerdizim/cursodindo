614c4a2fceb76746035728a5168d7bf0fab57d03
Cassio Mendes <cassio.henrique14@gmail.com>
2020-10-09 12:46:10 -0300

    Criando operacao de instanciacao

--- src/main/java/com/dindo/cursomc/CursodindoApplication.java
+++ src/main/java/com/dindo/cursomc/CursodindoApplication.java
@@ -1,13 +1,33 @@
 package com.dindo.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.dindo.cursomc.domain.Categoria;
+import com.dindo.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursodindoApplication {
+public class CursodindoApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursodindoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
